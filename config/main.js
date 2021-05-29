09b419fd4baf95d1d220e4ed97d383c5ed04c0b4
Hemadri Dasari <hemadri.dasari1990@gmail.com>
2021-05-29 20:33:02 +0400

    fixes

--- config/main.js
+++ config/main.js
@@ -1,23 +1,23 @@
 module.exports = {
   // Secret key for JWT signing and encryption
-  "accessTokenSecret": "ldrwngrt2021", // lets do retro with next generation retrospective tool
-  "refreshTokenSecret": "1202trgnwrdl", // access token secrete reverse
+  accessTokenSecret: "ldrwngrt2021", // lets do retro with next generation retrospective tool
+  refreshTokenSecret: "1202trgnwrdl", // access token secrete reverse
   // Database connection information
-  database: 'mongodb://localhost:27017/lets-do-retro',
+  database: "mongodb://localhost:27017/lets-do-retro",
   // Setting port for server
   port: 8080,
   // Configuring Mailgun API for sending transactional email
   // necessary in order to run tests in parallel of the main app
   test_port: 8080,
-  test_db: 'lets-do-retro',
-  test_env: 'test',
+  test_db: "lets-do-retro",
+  test_env: "test",
   bcryptSalt: 10,
-  "email": {
-    "from": "letsdoretro@gmail.com",
-    "host": "smtp.gmail.com",
-    "port": 587,
-    "user": "letsdoretro@gmail.com",
-    "password": "ziknjvfoihgdjabo" //nodemailer
+  email: {
+    from: "letsdoretro@gmail.com",
+    host: "smtp.gmail.com",
+    port: 587,
+    user: "letsdoretro@gmail.com",
+    password: "ziknjvfoihgdjabo", //nodemailer
   },
-  "url": "http://localhost:3000",
+  url: "http://www.letsdoretro.com",
 };
