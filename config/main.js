f21572f7c40d53334763427c83418f24180f7b5f
Hemadri Dasari <hemadri.dasari1990@gmail.com>
2021-06-04 11:19:34 +0400

    fixes

--- config/main.js
+++ config/main.js
@@ -3,7 +3,8 @@ module.exports = {
   accessTokenSecret: "ldrwngrt2021", // lets do retro with next generation retrospective tool
   refreshTokenSecret: "1202trgnwrdl", // access token secrete reverse
   // Database connection information
-  database: "mongodb://uat.letsdoretro.com:27017/lets-do-retro",
+  database:
+    "mongodb://hemadri:Hemanth@1505@cluster0-shard-00-00.vgtdr.mongodb.net:27017,cluster0-shard-00-01.vgtdr.mongodb.net:27017,cluster0-shard-00-02.vgtdr.mongodb.net:27017/lets-do-retro?ssl=true&replicaSet=atlas-elrkrn-shard-0&authSource=admin&retryWrites=true&w=majority",
   // Setting port for server
   port: 8080,
   // Configuring Mailgun API for sending transactional email
