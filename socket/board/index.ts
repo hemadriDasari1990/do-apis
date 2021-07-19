b317641be8a189d8658d397bff044934a8d3ebb9
Hemadri Dasari <hemadri.dasari1990@gmail.com>
2021-07-19 23:45:17 +0400

    resume session changes

--- socket/board/index.ts
+++ socket/board/index.ts
@@ -42,7 +42,6 @@ export default function board(io: socketio.Server, socket: Socket) {
 
   socket.on("resume-session", async (payload: { [Key: string]: any }) => {
     try {
-      console.log("payload", payload);
       const query: any = socket.handshake.query;
       await verifyToken(query?.token, io);
       const user: any = decodeToken(query?.token);
@@ -90,6 +89,14 @@ export default function board(io: socketio.Server, socket: Socket) {
     try {
       const boardDetails = await getBoardDetailsWithMembers(payload?.id, null);
       await io.emit(`board-details-response`, boardDetails);
+    } catch (err) {
+      return err;
+    }
+  });
+
+  socket.on("show-reaction", async (type: string) => {
+    try {
+      await io.emit(`show-reaction`, type);
     } catch (err) {
       return err;
     }
