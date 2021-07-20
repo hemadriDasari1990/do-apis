84ffa210fb27f6257e8831fd58aa5156bfca2a13
Hemadri Dasari <hemadri.dasari1990@gmail.com>
2021-07-20 07:07:16 +0400

    resume session changes

--- migrations/20210720030049-Add-Enable-Reactions-Column.js
+++ migrations/20210720030049-Add-Enable-Reactions-Column.js
@@ -1,13 +1,10 @@
 module.exports = {
   async up(db, client) {
-    db.getCollection("board").updateMany(
-      {},
-      { $set: { enableReactions: false } }
-    );
+    db.collection("board").updateMany({}, { $set: { enableReactions: false } });
   },
 
   async down(db, client) {
-    db.getCollection("board").updateMany(
+    db.collection("board").updateMany(
       {},
       { $unset: { enableReactions: false } }
     );
